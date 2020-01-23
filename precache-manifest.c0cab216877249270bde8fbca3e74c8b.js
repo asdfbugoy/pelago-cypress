640ca4a101e4fc0ca82363772b18b61efbc94ee3
Francis Samande Declaro <francis.declaro.092283@gmail.com>
2020-01-23 18:12:04 +0800

    Updates

---
+++ precache-manifest.c0cab216877249270bde8fbca3e74c8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7ff1be1671521fcc562552ea2218d9",
+    "url": "/pelago-cypress/index.html"
+  },
+  {
+    "revision": "27674db6516a4ea21c6f",
+    "url": "/pelago-cypress/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "26b1f8a90711d93c8c2f",
+    "url": "/pelago-cypress/static/js/2.b14e1435.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pelago-cypress/static/js/2.b14e1435.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27674db6516a4ea21c6f",
+    "url": "/pelago-cypress/static/js/main.1b8abd36.chunk.js"
+  },
+  {
+    "revision": "59505009f7a458db9bc3",
+    "url": "/pelago-cypress/static/js/runtime-main.1548ce77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pelago-cypress/static/media/logo.5d5d9eef.svg"
+  }
+]);
